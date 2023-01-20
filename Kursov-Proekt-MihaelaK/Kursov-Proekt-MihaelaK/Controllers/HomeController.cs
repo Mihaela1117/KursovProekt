aa5ff6563d23c4c908d83c758e7c455847893b2b
Mihaela1117 <107248062+Mihaela1117@users.noreply.github.com>
2023-01-20 10:03:11 +0200

    Add files via upload

---
+++ Kursov-Proekt-MihaelaK/Kursov-Proekt-MihaelaK/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Kursov_Proekt_MihaelaK.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Kursov_Proekt_MihaelaK.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
